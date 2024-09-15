d8ba707e11dd6562f9e33a7294eeb84d6243bb71
Nguyen1976 <nguyen2202794@gmail.com>
2024-09-15 21:13:51 +0700

    Done Custem Router

--- fe/ui-app/src/index.js
+++ fe/ui-app/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import RouterCustom from './router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Online shop</h1>
+  <BrowserRouter>
+    <RouterCustom />
+  </BrowserRouter>
 );
