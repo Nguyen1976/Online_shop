ed0803e8ebd398223e5549f7130f90b1c4e864f9
Nguyen1976 <nguyen2202794@gmail.com>
2024-09-20 22:43:37 +0700

    base responsive

--- fe/ui-app/src/pages/users/theme/header/index.js
+++ fe/ui-app/src/pages/users/theme/header/index.js
@@ -1,8 +1,69 @@
 import { memo } from 'react';
+import "./style.scss";
+import { AiOutlineFacebook, AiOutlineMail  } from "react-icons/ai";
+import { FiInstagram, FiLinkedin, FiGlobe, FiUser } from "react-icons/fi";
+import { Link } from 'react-router-dom';
+import { formatter } from 'utils/fomater';
 
 function Header() {
     return (  
-        <h1>Header</h1>
+        <>
+            <div className='header__top'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-6 header__top-left'>
+                            <ul>
+                                <li>
+                                    <AiOutlineMail />
+                                    hello@gmail.com
+                                </li>
+                                <li>
+                                    Miễn phí ship đơn từ {formatter(200000)}
+                                </li>
+                            </ul>
+                        </div>
+                        <div className='col-6 header__top-rigth'>
+                            <ul>
+                                <li>
+                                    <Link to={""}>
+                                        <AiOutlineFacebook />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={""}>
+                                        <FiInstagram />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={""}>
+                                        <FiLinkedin />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={""}>
+                                        <FiGlobe />
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={""}>
+                                        <FiUser />
+                                    </Link>
+                                    <span>Đăng nhập</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-lg-3 col-xl-3'>LOGO</div>
+                    <div className='col-lg-3 col-xl-3'>MENUS</div>
+                    <div className='col-lg-3 col-xl-3'>PHONE</div>
+                    <div className='col-lg-3 col-xl-3'>LOGO</div>
+                </div>
+            </div>
+        </>
     );
 }
 
