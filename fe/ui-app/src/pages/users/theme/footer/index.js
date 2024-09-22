4bbe749e834d7edb49b28ba9fdf7206e9df03b74
Nguyen1976 <nguyen2202794@gmail.com>
2024-09-22 11:10:12 +0700

    Done Footer and Responsive Footer

--- fe/ui-app/src/pages/users/theme/footer/index.js
+++ fe/ui-app/src/pages/users/theme/footer/index.js
@@ -8,7 +8,7 @@ function Footer() {
         <footer className='footer'>
             <div className='container'>
                 <div className='row'>
-                    <div className='col-lg-3'>
+                    <div className='col-lg-3 col-md-6 col-sm-6 col-xs-12'>
                         <div className='footer__about'>
                             <h1 className='footer__about_logo'>Sivi Shop</h1>
                             <ul>
@@ -18,7 +18,7 @@ function Footer() {
                             </ul>
                         </div>
                     </div>
-                    <div className='col-lg-6'>
+                    <div className='col-lg-6 col-md-6 col-sm-6 col-xs-12'>
                         <div className="footer__widget">
                             <h6>Cửa hàng</h6>
                             <ul>
@@ -57,12 +57,12 @@ function Footer() {
                             </ul>
                         </div>
                     </div>
-                    <div className='col-lg-3'>
+                    <div className='col-lg-3 col-md-12 col-sm-12 col-xs-12'>
                         <div className="footer__widget">
                             <h6>Khuyến mãi & ưu đãi</h6>
                             <p>Đăng ký nhận thông tin tại đây</p>
                             <form action="#">
-                                <div>
+                                <div className='input-group'>
                                     <input type="text" placeholder='Nhập Email' />
                                     <button type='submit' className='button-submit'>Đăng ký</button>
                                 </div>
